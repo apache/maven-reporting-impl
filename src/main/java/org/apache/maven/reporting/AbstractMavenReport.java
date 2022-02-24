b9de2b45c18c730a4c48bb144c9404917c270911
Guillaume Nodet <gnodet@gmail.com>
2022-02-24 17:00:58 +0100

    Switch a few core plugins to the new api

--- src/main/java/org/apache/maven/reporting/AbstractMavenReport.java
+++ src/main/java/org/apache/maven/reporting/AbstractMavenReport.java
@@ -132,7 +132,7 @@ public abstract class AbstractMavenReport
         siteContext.setLocale( locale );
         siteContext.setTemplateProperties( getTemplateProperties() );
 
-        RenderingContext context = new RenderingContext( outputDirectory, filename );
+        RenderingContext context = new RenderingContext( outputDirectory, filename, null );
 
         SiteRendererSink sink = new SiteRendererSink( context );
 
@@ -149,7 +149,7 @@ public abstract class AbstractMavenReport
                     new OutputStreamWriter( new FileOutputStream( new File( outputDirectory, filename ) ),
                                             getOutputEncoding() ) )
                 {
-                    getSiteRenderer().generateDocument( writer, sink, siteContext );
+                    getSiteRenderer().mergeDocumentIntoSite( writer, sink, siteContext );
                 }
             }
         }
